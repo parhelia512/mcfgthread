e987a1a7aceaebe99a96b8cf705e3e393952edcb
LIU Hao <lh_mouse@126.com>
2026-04-03 13:43:00 +0800

    *: Rectify license filename in copyright notices [no ci]

--- test/seh_terminate.c
+++ test/seh_terminate.c
@@ -2,7 +2,7 @@
  * Copyright (C) 2022-2026 LH_Mouse. All wrongs reserved.
  *
  * MCF Gthread is free software. Licensing information is included in
- * LICENSE.TXT as a whole. The GCC Runtime Library Exception applies
+ * LICENSE.md as a whole. The GCC Runtime Library Exception applies
  * to this file.  */
 
 #define WIN32_LEAN_AND_MEAN  1
